5dda69ccf9f0a85480865271039857d5789660d1
mtfalls <goldjet45@gmail.com>
2019-09-02 04:06:22 -0400

    round 3 var

--- gauntlet_template.py
+++ gauntlet_template.py
@@ -24,7 +24,7 @@ def check_gauntlet():
 
     # Use mechanize to set the locale's select value to 'en-US'
     br = mechanize.Browser()
-    br.set_handle_robots(False)
+    #br.set_handle_robots(False)
     #br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
     br.open(vg_url)
 
