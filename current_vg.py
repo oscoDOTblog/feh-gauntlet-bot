88b6645903e00e17b38aec7554e3d1e3e6e87acb
Oscar K. Sandoval Rivera <mtfallsVR@atemosta.com>
2020-08-03 11:55:50 -0400

    Update to correct date

--- current_vg.py
+++ current_vg.py
@@ -1,11 +1,11 @@
 vg_url='https://support.fire-emblem-heroes.com/voting_gauntlet/tournaments/38'
 vg_hashtag='#SD'
-round_1_start_raw = 'Aug 01 2020 3:00AM'
-round_1_end_raw = 'Aug 03 2020 12:00AM'
-round_2_start_raw = 'Aug 03 2020 3:00AM'
-round_2_end_raw = 'Aug 05 2020 12:00AM'
-round_3_start_raw = 'Aug 05 2020 3:00AM'
-round_3_end_raw = 'Aug 07 2020 12:00AM'
+round_1_start_raw = 'July 31 2020 3:00AM'
+round_1_end_raw = 'Aug 02 2020 12:00AM'
+round_2_start_raw = 'Aug 02 2020 3:00AM'
+round_2_end_raw = 'Aug 04 2020 12:00AM'
+round_3_start_raw = 'Aug 04 2020 3:00AM'
+round_3_end_raw = 'Aug 06 2020 12:00AM'
 round_1_unit_1 = "Mia"
 round_1_unit_2 = "Rhys"
 round_1_unit_3 = "Lute"
