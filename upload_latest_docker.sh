503e89da4c7a38f44638f51964fd44e56b3405d1
Ousikai <mtfallsVR@protonmail.com>
2022-07-01 08:57:33 -0400

    Config Update

--- upload_latest_docker.sh
+++ upload_latest_docker.sh
@@ -1,5 +1,5 @@
 # Set Docker Image Version
-export DOCKER_VERSION=1.59
+export DOCKER_VERSION=1.61
 
 # Build, Deploy, and Tear Down Docker Containers
 echo "Attempting to build Docker containers..."
@@ -39,11 +39,11 @@ docker push atemosta/feh-gauntlet-bot:rebecca-latest
 echo "----------Success!!!----------"
 
 
-# Clear Docker Containers and Images
-echo "Clearing old docker images..."
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-export IMAGE_PATTERN=feh-gauntlet-bot
-docker images -a |  grep ${IMAGE_PATTERN}
-docker images -a | grep ${IMAGE_PATTERN} | awk '{print $3}' | xargs docker rmi -f
-echo "----------Success!!!----------"
+# Clear Docker Containers and Images (Optional)
+# echo "Clearing old docker images..."
+# docker stop $(docker ps -a -q)
+# docker rm $(docker ps -a -q)
+# export IMAGE_PATTERN=feh-gauntlet-bot
+# docker images -a |  grep ${IMAGE_PATTERN}
+# docker images -a | grep ${IMAGE_PATTERN} | awk '{print $3}' | xargs docker rmi -f
+# echo "----------Success!!!----------"
