660f303e20f5077057cd4eacb6915ba83f9a26be
Oscar Kevin <mtfallsVR@atemosta.com>
2022-07-15 19:54:59 -0400

    Pipfile and Pipfile.lock Updates (#77)
    * install pipenv for convoy
    
    * create pipfile for genny
    
    * add pipfile for rebecca
    
    * update Pipfile versions
    
    * Dockerfile updates for services
    
    * upgrade pipenv
    
    * update pipfile.lock
    
    * rename discord admin role
    
    * update docker compose verbage

--- deploy_docker_services.sh
+++ deploy_docker_services.sh
@@ -7,6 +7,6 @@ echo "Successfully copied secret credentials!"
 
 # Run Latest Docker Images
 echo "Attempting to update Docker images..."
-docker-compose down
-docker-compose up -d
+docker compose down
+docker compose up -d
 echo "Successfully updated Docker images!"
