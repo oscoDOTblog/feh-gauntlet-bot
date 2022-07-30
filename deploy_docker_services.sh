2051333ca153dc25caca782ade200e60877f7397
Ousikai <mtfallsVR@protonmail.com>
2022-07-30 09:29:03 -0400

    add services names

--- deploy_docker_services.sh
+++ deploy_docker_services.sh
@@ -8,5 +8,6 @@ echo "Successfully copied secret credentials!"
 # Run Latest Docker Images
 echo "Attempting to update Docker images..."
 docker compose down
+docker compose build
 docker compose up -d
 echo "Successfully updated Docker images!"
