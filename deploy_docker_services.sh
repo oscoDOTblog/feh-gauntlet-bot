73a93df4a3890de48046ac1ace1a3a648301a45f
Oscar Kevin <mtfallsVR@atemosta.com>
2022-08-03 09:19:07 -0400

    Add Service Names to Docker Deployments (#81)
    * add services names
    
    * names donezo
    
    * Update config.py

--- deploy_docker_services.sh
+++ deploy_docker_services.sh
@@ -8,5 +8,8 @@ echo "Successfully copied secret credentials!"
 # Run Latest Docker Images
 echo "Attempting to update Docker images..."
 docker compose down
+docker compose build
 docker compose up -d
+docker compose -f docker-compose.yml restart genny
+docker compose -f docker-compose.yml restart rebecca
 echo "Successfully updated Docker images!"
